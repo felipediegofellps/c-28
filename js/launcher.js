833136c2efa57c0b68ff9bbb696e5586d6732915
felipediegofellps <101751056+felipediegofellps@users.noreply.github.com>
2022-05-30 17:51:01 -0300

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,27 @@
+class Launcher{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+    
+        }   
+        this.bodyA = bodyA
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    holdBack(body){
+        this.launcher.bodyA = body;
+    }
+
+    display() { if(this.launcher.bodyA) 
+    { var pointA=this.bodyA.position; var pointB=this.pointB; strokeWeight(2);
+     line(pointA.x,pointA.y,pointB.x,pointB.y); } }
+}
